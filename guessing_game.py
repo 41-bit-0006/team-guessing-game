350143a86911cb2fd4cc8d1c745ed054376e65d6
ashan0708 <ashanmanusha182@gmail.com>
2025-05-03 01:38:57 +0530

    Added hints for guesses

--- guessing_game.py
+++ guessing_game.py
@@ -3,6 +3,11 @@ while True:
     print("Guess a number between 1 and 100")
     guess = int(input())
 
+    if guess < number:
+     print("Too low!")
+    elif guess > number:
+     print("Too high!")
+
     if guess == number:
         print("You win!")
     else:
